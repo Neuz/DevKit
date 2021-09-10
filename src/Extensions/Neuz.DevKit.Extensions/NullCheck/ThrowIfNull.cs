7419d989beb7e7bb8b14c37ef9fad0d39b7c727e
viacooky <viacooky@qq.com>
2021-09-10 20:41:02 +0800

    feat: 增加空值检查

---
+++ src/Extensions/Neuz.DevKit.Extensions/NullCheck/ThrowIfNull.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Neuz.DevKit.Extensions
+{
+    public static class NullCheck
+    {
+        /// <summary>
+        /// 如果为空抛出 <see cref="ArgumentNullException"/>
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="message"></param>
+        public static void ThrowIfNull(object @this, string message=null)
+        {
+            message ??= nameof(@this);
+            if (@this == null) throw new ArgumentNullException(message);
+        }
+
+        /// <summary>
+        /// 如果为空抛出 <see cref="ArgumentNullException"/>
+        /// </summary>
+        /// <param name="objects"></param>
+        public static void ThrowIfNull(params object[] objects)
+        {
+            foreach (var o in objects)
+            {
+                ThrowIfNull(o);
+            }
+        }
+    }
+}
