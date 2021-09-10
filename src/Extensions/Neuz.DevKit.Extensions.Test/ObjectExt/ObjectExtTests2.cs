32fb169bb3c02c1d1ba1960af5579dccd2a594cf
viacooky <viacooky@qq.com>
2021-09-10 20:42:07 +0800

    feat: 增加 Object.IsClass()

---
+++ src/Extensions/Neuz.DevKit.Extensions.Test/ObjectExt/ObjectExtTests2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Extensions.Contracts;
+
+// ReSharper disable ArrangeDefaultValueWhenTypeNotEvident
+// ReSharper disable RedundantTypeArgumentsOfMethod
+
+// ReSharper disable ExpressionIsAlwaysNull
+
+namespace Neuz.DevKit.Extensions.Test
+{
+    [TestClass()]
+    public class ObjectExtTests2
+    {
+        [ContractTestCase]
+        public void To_Test_1()
+        {
+            "Object.IsClass".Test(() =>
+            {
+                var obj1 = new MyClass();
+                Assert.IsTrue(obj1.IsClass());
+
+                var obj2 = 1233;
+                Assert.IsFalse(obj2.IsClass());
+
+                var obj3 = typeof(string);
+                Assert.IsTrue(obj3.IsClass());
+            });
+        }
+
+        public class MyClass
+        {
+            
+        }
+    }
+}
---
+++ src/Extensions/Neuz.DevKit.Extensions/ObjectExt/IsClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Neuz.DevKit.Extensions
+{
+    public static partial class ObjectExt
+    {
+        /// <summary>
+        ///     当前对象类型是否 Class
+        /// </summary>
+        /// <param name="this"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var obj1 = new MyClass();
+        /// obj1.IsClass(); // true
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var obj2 = 12;
+        /// obj2.IsClass(); // false
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// object obj3 = null;
+        /// obj3.IsClass(); // false
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static bool IsClass(this object @this)
+        {
+            NullCheck.ThrowIfNull(@this);
+            return @this.GetType().IsClass;
+        }
+    }
+}
