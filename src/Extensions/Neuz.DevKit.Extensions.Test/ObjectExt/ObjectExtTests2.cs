c6ddf094b251b420cb34231e8bc1e3f9ff03a5d7
viacooky <viacooky@qq.com>
2021-09-10 22:03:15 +0800

    feat: Object.IsEnum()

--- src/Extensions/Neuz.DevKit.Extensions.Test/ObjectExt/ObjectExtTests2.cs
+++ src/Extensions/Neuz.DevKit.Extensions.Test/ObjectExt/ObjectExtTests2.cs
@@ -87,6 +87,27 @@ namespace Neuz.DevKit.Extensions.Test
                 object obj4 = new MyClass2();
                 Assert.IsTrue(obj4.IsSerializable());
             });
+
+            "Object.IsEnum()".Test(() =>
+            {
+                var obj1 = new[] {"1", "2"};
+                Assert.IsFalse(obj1.IsEnum());
+
+                object obj2 = null;
+                Assert.IsFalse(obj2.IsEnum());
+
+                object obj3 = new MyClass();
+                Assert.IsFalse(obj3.IsEnum());
+
+                object obj4 = MyEnum.First;
+                Assert.IsTrue(obj4.IsEnum());
+            });
+        }
+
+
+        public enum MyEnum
+        {
+            First
         }
 
         public class MyClass
---
+++ src/Extensions/Neuz.DevKit.Extensions/ObjectExt/IsEnum.cs
@@ -0,0 +1,28 @@
+﻿namespace Neuz.DevKit.Extensions
+{
+    public static partial class ObjectExt
+    {
+        /// <summary>
+        ///     当前对象类型是否枚举
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns><paramref name="this" /> 为 null ，返回 false</returns>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// private enum MyEnum
+        /// {
+        ///     None
+        /// }
+        /// 
+        /// var obj1 = MyEnum.None;
+        /// obj1.IsEnum(); // true
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static bool IsEnum(this object @this)
+        {
+            return @this != null && @this.GetType().IsEnum;
+        }
+    }
+}
