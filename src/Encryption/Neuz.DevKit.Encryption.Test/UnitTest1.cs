ba53748c6eb2128d7b787df2d507c3a47385b223
viacooky <viacooky@qq.com>
2021-09-12 00:16:27 +0800

    feat: init Encryption Project

---
+++ src/Encryption/Neuz.DevKit.Encryption.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neuz.DevKit.Encryption.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
