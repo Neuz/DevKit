84f4bbf280500f2ec5431c45266d98d5ef6376e0
viacooky <viacooky@qq.com>
2021-09-11 17:54:48 +0800

    feat: 增加 Enum 相关

---
+++ src/Extensions/Neuz.DevKit.Extensions.Test/EnumExt/EnumExtTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Extensions.Contracts;
+
+namespace Neuz.DevKit.Extensions.Test
+{
+    [TestClass]
+    public class EnumExtTests
+    {
+        [ContractTestCase]
+        public void DataTable_Test_1()
+        {
+            "Enum.GetDescription() - 1".Test(() =>
+            {
+                Assert.AreEqual("First Description", MyEnum.First.GetDescription());
+            });
+        }
+
+        public enum MyEnum
+        {
+            [System.ComponentModel.Description("First Description")]
+            First = 0
+        }
+    }
+}
