dbca2421594139379b7c2e3c008b4106e35b5ac9
viacooky <viacooky@qq.com>
2021-09-11 17:02:10 +0800

    feat: 增加 DataTable.FirstRow()

---
+++ src/Extensions/Neuz.DevKit.Extensions/DataTableExt/FirstRow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+
+namespace Neuz.DevKit.Extensions
+{
+    public static partial class DataTableExt
+    {
+        /// <summary>
+        ///     获取首行
+        /// <para>
+        /// 当前DataTable没有行时，返回null
+        /// </para>
+        /// </summary>
+        /// <param name="this"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns></returns>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// dataTable.FirstRow();
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static DataRow FirstRow(this DataTable @this)
+        {
+            return !@this.HasRows() ? null : @this.Rows[0];
+        }
+    }
+}
