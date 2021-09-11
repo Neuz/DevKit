da9ef1bf2746265631cdce3804f3fe955549f8fa
viacooky <viacooky@qq.com>
2021-09-11 16:48:26 +0800

    feat: 增加 DataRow.Cell<T>()

---
+++ src/Extensions/Neuz.DevKit.Extensions.Test/DataRowExt/Cell.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Extensions.Contracts;
+
+// ReSharper disable ExpressionIsAlwaysNull
+// ReSharper disable UnusedParameter.Local
+
+namespace Neuz.DevKit.Extensions.Test
+{
+    [TestClass]
+    public class DataRowExtTests
+    {
+        [ContractTestCase]
+        public void Cell_Test_1()
+        {
+            "DataRow.Cell<T>() - 1".Test(() =>
+            {
+                var dataTable = new DataTable();
+                dataTable.Columns.Add("id");
+                dataTable.Columns.Add("name");
+                dataTable.Rows.Add("aaa", "123");
+
+                Assert.AreEqual(default, dataTable.Rows[0].Cell<string>("columnName"));
+                Assert.AreEqual(123, dataTable.Rows[0].Cell<int>("name"));
+            });
+
+            "DataRow.Cell<T>() - 2".Test(() =>
+            {
+                var dataTable = new DataTable();
+                dataTable.Columns.Add("id");
+                dataTable.Columns.Add("name");
+                dataTable.Rows.Add("aaa", "123");
+
+                Assert.AreEqual("aaa", dataTable.Rows[0].Cell("columnName", "aaa"));
+
+                Assert.AreEqual("aaa - 1", dataTable.Rows[0].Cell("columnName", row => $"{row["id"]} - 1"));
+
+            });
+        }
+    }
+}
---
+++ src/Extensions/Neuz.DevKit.Extensions/DataRowExt/Cell.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+
+namespace Neuz.DevKit.Extensions
+{
+    public static partial class DataRowExt
+    {
+        /// <summary>
+        ///     获取当前行中某个列的值
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="columnName">列名</param>
+        /// <typeparam name="T">目标类型，约束为 <see cref="IConvertible" /></typeparam>
+        /// <returns><paramref name="columnName" />不存在 或 转换失败，返回 default(<typeparamref name="T" />)</returns>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var rs = dataRow.Cell<string>("columnName"); // if fail return default(string)
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static T Cell<T>(this DataRow @this, string columnName) where T : IConvertible
+        {
+            return @this.Cell(columnName, default(T));
+        }
+
+        /// <summary>
+        ///     获取当前行中某个列的值
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="columnName">列名</param>
+        /// <param name="func"></param>
+        /// <typeparam name="T">目标类型，约束为 <see cref="IConvertible" /></typeparam>
+        /// <returns><paramref name="columnName" />不存在 或 转换失败，返回 <paramref name="func" /></returns>
+        public static T Cell<T>(this DataRow @this, string columnName, Func<DataRow,T> func) where T : IConvertible
+        {
+            return @this.Cell(columnName, func.Invoke(@this));
+        }
+
+        /// <summary>
+        ///     获取当前行中某个列的值
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="columnName">列名</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <typeparam name="T">
+        ///     目标类型，约束为 <see cref="IConvertible" />
+        /// </typeparam>
+        /// <returns><paramref name="columnName" />不存在 或 转换失败，返回 <paramref name="defaultValue" /></returns>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var rs = dataRow.Cell("columnName", "abc"); // if fail return "abc"
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static T Cell<T>(this DataRow @this, string columnName, T defaultValue) where T : IConvertible
+        {
+            return @this.Table.Columns.Contains(columnName)
+                ? @this[columnName].To(defaultValue)
+                : defaultValue;
+        }
+    }
+}
