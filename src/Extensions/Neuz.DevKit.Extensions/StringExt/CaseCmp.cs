7de4ce5346fb2e2b43837a5e3af2b6b861c628ac
viacooky <viacooky@qq.com>
2021-09-17 14:34:58 +0800

    feat: 增加 String.CaseCmp()

---
+++ src/Extensions/Neuz.DevKit.Extensions/StringExt/CaseCmp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Neuz.DevKit.Extensions
+{
+    public static partial class StringExt
+    {
+        /// <summary>
+        /// 忽略大小写比较
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="value">需要进行比较的字符串</param>
+        /// <param name="comparison">默认 <see cref="StringComparison.CurrentCultureIgnoreCase"/></param>
+        /// <returns></returns>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// "aaa".CaseCmp("AaA"); // true
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static bool CaseCmp(this string @this, string value, StringComparison comparison = StringComparison.CurrentCultureIgnoreCase)
+        {
+            return string.Equals(@this, value, comparison);
+        }
+    }
+}
