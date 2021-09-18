3db420333fc2b7629caa4a32c6650afd16187144
viacooky <viacooky@qq.com>
2021-09-18 19:27:50 +0800

    feat: 增加 Decimal.Abs()

---
+++ src/Extensions/Neuz.DevKit.Extensions.Test/DecimalExt/DecimalExtTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Extensions.Contracts;
+
+namespace Neuz.DevKit.Extensions.Test
+{
+    [TestClass]
+    public class DecimalExtTests
+    {
+        [ContractTestCase]
+        public void Abs_Test_1()
+        {
+            "Decimal.Abs() - 1".Test<decimal, decimal>((source, result) =>
+                               {
+                                   Assert.AreEqual(result, source.Abs());
+                               })
+                               .WithArguments(
+                                   (-1.32m, 1.32m),
+                                   (decimal.MinValue, -decimal.MinValue),
+                                   (decimal.MaxValue, decimal.MaxValue)
+                               );
+        }
+    }
+}
---
+++ src/Extensions/Neuz.DevKit.Extensions/DecimalExt/Abs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Neuz.DevKit.Extensions
+{
+    public static partial class DecimalExt
+    {
+        /// <summary>
+        /// 绝对值
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var d = -123m;
+        /// d.Abs();
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static decimal Abs(this decimal @this)
+        {
+            return Math.Abs(@this);
+        }
+    }
+}
