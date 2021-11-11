37ad5d43b51e5f8364c246442fbabdc3a4c744eb
viacooky <viacooky@qq.com>
2021-11-11 14:29:18 +0800

    feat: remove [Result]

---
+++ src/DotNetVersions/Neuz.DevKit.DotNetVersions/DotNetVersion.cs
@@ -0,0 +1,17 @@
+﻿namespace Neuz.DevKit
+{
+    public class DotNetVersions
+    {
+        public DotNetVersion C { get; set; }
+
+
+        public class DotNetVersion
+        {
+            public string Version { get; }
+
+            public string SpLevel { get; }
+
+            public bool Installed { get; }
+        }
+    }
+}
